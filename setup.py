4a1aa873d73f7954544732a5f1dff7187ef9d7bf
rubesh29 <rubesh.programmer@gmail.com>
2025-01-13 15:14:48 +0100

    pushing setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+from distutils.core import setup
+setup(
+ name='todo_sheduler',
+ version='1.0',
+ author='rubesh29',
+ license='MIT',
+ long_description=open('README.md').read(),
+)
